4f0ffb36e8f04d03d509544b09e36aa136b2e4b2
IlyaKuznetsov <makakaataka@gmail.com>
2022-09-16 00:20:32 +0300

    99999

--- main.js
+++ main.js
@@ -106,17 +106,25 @@
  
 // This
 
-function Person(fn, ln) {
-	this.first_name = fn;
-	this.last_name = ln;
+// function Person(fn, ln) {
+// 	this.first_name = fn;
+// 	this.last_name = ln;
 
-	this.displayName = function() {
-		console.log(`Name: ${this.first_name} ${this.last_name}`);
-	}
+// 	this.displayName = function() {
+// 		console.log(`Name: ${this.first_name} ${this.last_name}`);
+// 	}
+// }
+
+// let person = new Person("John", "Reed");
+// person.displayName();  // Prints Name: John Reed
+// let person2 = new Person("Paul", "Adams");
+// person2.displayName();  // Prints Name: Paul Adams
+ 
+
+// TypeOf
+
+function typeOfSum(a, b) {
+	
+	return typeof(a + b);
+
 }
-
-let person = new Person("John", "Reed");
-person.displayName();  // Prints Name: John Reed
-let person2 = new Person("Paul", "Adams");
-person2.displayName();  // Prints Name: Paul Adams
-
